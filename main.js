84aa91345befe1bfe357b6607b0a88a509e85f22
pynezz <kadam@pm.me>
2021-09-20 20:12:46 +0200

    fixed a bug, seems to be working now

--- main.js
+++ main.js
@@ -18,12 +18,24 @@ function addImg(x, y) {
     let tile = document.getElementById(`${x}_${y}`);
     tile.removeAttribute("onclick");
     if (player1playing) {
-        sumPlayer1Values(x, y);
+        player1XValue += x;
+        player1YValue += y;
+        
+        console.log('player 1 x: ' + player1XValue);
+        console.log('player 1 y: ' + player1YValue);
+        //sumPlayer1Values(x, y);
         tile.classList.add("setPlayer1");
+        tile.classList.remove("cellHover");
     }
     else {
-        sumPlayer2Values(x, y);
+        //sumPlayer2Values(x, y);
+        player2XValue += x;
+        player2YValue += y;
+        console.log('player 2 x: ' + player2XValue);
+        console.log('player 2 y: ' + player2YValue);
         tile.classList.add("setPlayer2");
+        tile.classList.remove("cellHover");
+        checkWin();
     }
     checkWin();
     
@@ -32,23 +44,36 @@ function addImg(x, y) {
 }
 
 let win = false;
+
 function checkWin() {
     if (turnsPlayed > 4) {
-        if ((player1XValue + player1YValue) % 3 == 0) {
+        if (player1XValue % 3 === 0 && player1YValue % 3 === 0) {
             alert("Player 1 won!");
         }
-        else if ((player2XValue + player2YValue) % 3 == 0) {
+        else if (player2XValue % 3 === 0 && player2YValue % 3 === 0) {
             alert("Player 2 won!"); 
         }
     }
 }
 
-function sumPlayer1Values(x, y) {
-    player1YValue += y;
-    player1XValue += x;
-}
 
-function sumPlayer2Values(x, y) {
-    player2XValue += x;
-    player2YValue += y;
-}
+// function checkWin() {
+//     if (turnsPlayed > 4) {
+//         if ((player1XValue + player1YValue) % 3 == 0) {
+//             alert("Player 1 won!");
+//         }
+//         else if ((player2XValue + player2YValue) % 3 == 0) {
+//             alert("Player 2 won!"); 
+//         }
+//     }
+// }
+
+// function sumPlayer1Values(x, y) {
+//     player1YValue += y;
+//     player1XValue += x;
+// }
+
+// function sumPlayer2Values(x, y) {
+//     player2XValue += x;
+//     player2YValue += y;
+// }
